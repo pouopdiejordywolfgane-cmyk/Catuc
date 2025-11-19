88a4fc86a115711d4ce3a0952204669dc0045da6
pouopdiejordywolfgane-cmyk <pouopdiejordywolfgane@gmail.com>
2025-11-19 20:21:28 +0100

    Add files via upload

---
+++ Guest List.py
@@ -0,0 +1,6 @@
+
+guests = ["Albert Einstein", "Nelson Mandela", "Marie Curie"]
+
+print("Dear " + guests[0] + ", you are invited to dinner.")
+print("Dear " + guests[1] + ", you are invited to dinner.")
+print("Dear " + guests[2] + ", you are invited to dinner.")
